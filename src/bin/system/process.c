ade77c1db87b8a165b7141f2f513d19dd4c7d2dd
Alastair Poole <netstar@gmail.com>
2021-02-02 14:18:05 +0000

    fbsd: run time?
    This is right?

--- src/bin/system/process.c
+++ src/bin/system/process.c
@@ -929,10 +929,9 @@ _proc_thread_info(struct kinfo_proc *kp, Eina_Bool is_thread)
 
    usage = &kp->ki_rusage;
 
-   p->cpu_time = (usage->ru_utime.tv_sec * 1000000) + usage->ru_utime.tv_usec +
-       (usage->ru_stime.tv_sec * 1000000) + usage->ru_stime.tv_usec;
-   // XXX: See kern.sched.idlespins
-   p->cpu_time /= 10000;
+   p->cpu_time = ((usage->ru_utime.tv_sec * 1000000) + usage->ru_utime.tv_usec +
+       (usage->ru_stime.tv_sec * 1000000) + usage->ru_stime.tv_usec) / 10000;
+   p->run_time = (kp->ki_runtime + 500000) / 1000000;
    p->state = _process_state_name(kp->ki_stat);
    p->mem_virt = kp->ki_size;
    p->mem_rss = MEMSZ(kp->ki_rssize) * MEMSZ(pagesize);
